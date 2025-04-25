cc8c647bda274b16183d7d31e769bf985d450214
Hubert Jakubowski <264836@student.pwr.edu.pl>
2025-04-25 14:57:20 +0200

    Iterative implementation. It works, keeps nlogn complexity, but takes quite a lot of time to compile.

--- First Project/quicksort.hpp
+++ First Project/quicksort.hpp
@@ -1,51 +1,51 @@
 #pragma once
-#include <algorithm> 
+#include <algorithm>
 #include <functional>
+#include <random>
 
+// G³ówna funkcja quickSort
 template <typename E, typename Compare>
 void quickSort(E* arr, int size, const Compare& less) {
     if (size <= 1) return;
     quickSortStep(arr, 0, size - 1, less);
 }
 
+// Funkcja pomocnicza quickSortStep z losowym pivotem i iteracyjn¹ optymalizacj¹
 template <typename E, typename Compare>
 void quickSortStep(E* arr, int left, int right, const Compare& less) {
-    if (left >= right) return;
+    // Generator losowy do wyboru pivota
+    std::mt19937 generator(std::random_device{}());
 
-    
-    int mid = left + (right - left) / 2;
-    E first = arr[left];
-    E middle = arr[mid];
-    E last = arr[right];
+    while (left < right) {
+        // Losowy wybór pivota
+        std::uniform_int_distribution<int> dist(left, right);
+        int pivotIndex = dist(generator);
+        std::swap(arr[pivotIndex], arr[right]);
 
-   
-    E pivot = (less(first, middle)
-        ? (less(middle, last) ? middle : (less(first, last) ? last : first))
-        : (less(first, last) ? first : (less(middle, last) ? last : middle)));
+        E pivot = arr[right];
+        int l = left;
+        int r = right - 1;
 
-    
-    if (pivot == middle) {
-        std::swap(arr[mid], arr[right]);
+        // Podzia³ tablicy na elementy mniejsze i wiêksze od pivota
+        while (l <= r) {
+            while (l <= r && !less(pivot, arr[l])) l++;
+            while (r >= l && less(pivot, arr[r])) r--;
+            if (l < r) std::swap(arr[l], arr[r]);
+        }
+
+        std::swap(arr[l], arr[right]);
+
+        // Optymalizacja: mniejszy podzia³ rekurencyjny, wiêkszy iteracyjny
+        if (l - left < right - l) {
+            quickSortStep(arr, left, l - 1, less);
+            left = l + 1;
+        }
+        else {
+            quickSortStep(arr, l + 1, right, less);
+            right = l - 1;
+        }
     }
-    else if (pivot == first) {
-        std::swap(arr[left], arr[right]);
-    }
-
-    int l = left;
-    int r = right - 1;
-
-    while (l <= r) {
-        while (l <= r && !less(pivot, arr[l])) l++;
-        while (r >= l && !less(arr[r], pivot)) r--;
-        if (l < r) std::swap(arr[l], arr[r]);
-    }
-
-    std::swap(arr[l], arr[right]);
-
-    quickSortStep(arr, left, l - 1, less);
-    quickSortStep(arr, l + 1, right, less);
 }
-
 
 template <typename E>
 void quickSort(E* arr, int size) {
